002f4c5d2e72c5a4a6b39ab01fcdab0ee54b5da7
Saulo Ricci <sauloricci@Saulos-MacBook-Pro.local>
2014-06-15 00:02:45 -0300

    Adding the script that creates default directories to the crawl run

---
+++ scripts/set_dirs.sh
@@ -0,0 +1,9 @@
+# Script that creates some basic directories to run the crawler
+# credentials: foursquare user credentials that will be used in her behalf by the crawler.
+# data: contains files with the ids of users from foursquare who will be crawled.
+#output: where all crawled data will be saved
+#log: where all log will be stored
+mkdir ../credentials
+mkdir ../data
+mkdir ../output
+mkdir ../log
